440dd461f8844e3e9d94f3eda5e690f9e651d9b6
Luis Espinoza <Luis.A.Espinoza@jpl.nasa.gov>
2022-08-25 11:59:19 -0700

    Update github.py

--- github.py
+++ github.py
@@ -6,24 +6,41 @@ from util import LBEvent
 class LBGitHub:
     # max 100
     per_page = 30
+    timeout = 15
 
     @staticmethod
-    def commits(repo_url, branch="", token=None):
-        if repo := LBGitHub.parse(repo_url):
-            if response := LBGitHub.API.request(
-                endpoint=LBGitHub.endpoint(repo, "repos", "commits"),
-                headers=LBGitHub.auth(token),
-                params={"per_page": LBGitHub.per_page, "sha": branch},
-            ):
-                return [{
-                    "sha": val["sha"],
-                    "date": val["commit"]["author"]["date"],
-                    "author": val["author"]["login"],
-                    "url": val["html_url"]
-                } for val in response]
-        else:
-            LBEvent.error("LBGitHub.commits", "Error parsing URL")
-            return False
+    def branches(url, token=None):
+        # Call
+        if response := LBGitHub.query(url, resource="branches", token=token):
+            # Results
+            return [{
+                "name": val["name"],
+                "sha": val["commit"]["sha"],
+                "url": val["commit"]["url"],
+            } for val in response]
+
+    @staticmethod
+    def commits(url, branch="", token=None):
+        if response := LBGitHub.query(url, branch, "commits", token):
+            return [{
+                "sha": val["sha"],
+                "date": val["commit"]["author"]["date"],
+                "author": val["author"]["login"],
+                "url": val["html_url"],
+            } for val in response]
+
+    @staticmethod
+    def repos(url, token=None):
+        if response := LBGitHub.query(url, token=token):
+            return {
+                "name": response["name"],
+                "owner": response["owner"]["login"],
+                "description": response["description"],
+                "url": response["html_url"],
+                "created_at": response["created_at"],
+                "updated_at": response["updated_at"],
+                "pushed_at": response["pushed_at"],
+            }
 
     @staticmethod
     def parse(url):
@@ -58,24 +75,29 @@ class LBGitHub:
         return {"Authorization": f"token {token}"} if token else {}
 
     @staticmethod
-    def endpoint(repo, parent, child):
-        return f"https://{repo['base_url']}/{parent}/{repo['owner']}/{repo['name']}/{child}"
+    def endpoint(repo, resource):
+        endpoint = f"https://{repo['base_url']}/repos/{repo['owner']}/{repo['name']}"
+        if resource:
+            endpoint += f"/{resource}"
+        return endpoint
 
-    class API:
-        @staticmethod
-        def request(endpoint, headers, params):
-            try:
-                response = requests.get(
-                    endpoint,
-                    headers=headers,
-                    params=params,
-                    timeout=15,
-                )
+    @staticmethod
+    def query(url, branch="", resource="", token=None):
+        # Parse
+        if repo := LBGitHub.parse(url):
+            # Call
+            if response := requests.get(
+                LBGitHub.endpoint(repo, resource),
+                headers=LBGitHub.auth(token),
+                params={"per_page": LBGitHub.per_page, "sha": branch},
+                timeout=LBGitHub.timeout,
+            ):
+                # Results
                 if response.status_code == 200:
                     return response.json()
                 else:
                     LBEvent.error("LBGitHub.API", f"[{response.status_code}] {response.reason}")
                     return False
-            except Exception as e:
-                LBEvent.error("LBGitHub.API", e)
-                return False
+        else:
+            LBEvent.error("LBGitHub.query", "Error parsing URL")
+            return False
