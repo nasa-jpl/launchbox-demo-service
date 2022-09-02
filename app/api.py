ede9a58b97cbdaef9906f8662c576a4f1e57f972
Addison Hardy <hardy.addison@gmail.com>
2022-09-01 20:10:41 -0700

    Updated demo API endpoints

--- app/api.py
+++ app/api.py
@@ -7,12 +7,20 @@ import bottle
 app = bottle.Bottle()
 version = 1.0
 
+# Debug
+bottle.debug(False)
+
 # Index
 @app.get("/")
 def index():
     return {
         "api": "lb",
-        "env": os.environ.copy(),
+        "env": os.environ["ENVIRONMENT"],
         "type": "demo",
         "version": version,
     }
+
+# Resources
+@app.get("/resources")
+def index():
+    return {key: value for key, value in os.environ.items() if key.startswith("LB_")}
